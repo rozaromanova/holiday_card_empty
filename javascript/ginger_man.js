b98d08a7ca571258d7c895818be886b77e6d672c
Roza Romanova <roza.romanova@gmail.com>
2019-12-17 15:25:32 +0100

    first commit

---
+++ javascript/ginger_man.js
@@ -0,0 +1,75 @@
+/*
+10. First we need the 'ginger man factory' that will produce the ginger man div
+elements  with for us. The elements would need to have class so that we can
+style them later.
+*/
+
+class GingerMan {
+  constructor() {
+
+    return;
+    /*
+    18. We want now to give the ginger men the hat. For that, let's introduce the
+    variations of the hats the ginger men can have(with the pictures)
+    */
+
+    /*
+    23. Now the time for neck accessory. Add the variations just like we did above
+    */
+
+    /*
+    27. Time for the buttons - you know what to do(see above!)
+    */
+
+
+  };
+
+  /*
+  11. Now when we have a factory, we need a way to ask the factory to give us
+  the ginger man. (Go to index.html afterwards)
+  */
+
+  getGingerMan() {
+    return;
+  };
+
+  /*
+  15. On this step we want to create a way to tell the factory to bake the
+  chocolate ginger men. Once we did it, let's head to index.html and place
+  some chocolate ginger men on the tray
+  */
+
+  setChocolate() {
+    return;
+  };
+
+  /*
+  19. Once we have the types of the hat, let's create a wall to tell which hat
+  we want on the ginger men. Go to index.html to place the hats
+  */
+  setHat(hat) {
+    return;
+  };
+  /*
+  24. And just as we did with the hats, we want to have a way to tell which neck
+  accessory do we want(and then add it in index.html)
+  */
+  setNeck(neck) {
+    return;
+  };
+
+  /*
+  28. Define the way to tell which buttons do you want (and then add it in index.html)
+  */
+  setButton(button) {
+    return;
+  };
+
+  /*
+  32. Define the way to add the candy (and then add it in index.html)
+  */
+
+  setCandy() {
+    return;
+  };
+}
